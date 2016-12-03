315d12e2da97c2c354d9a133858dfad31807d121
BuildTools <unconfigured@null.spigotmc.org>
2016-12-03 17:36:50 -0500

    added numerica captcha dependencies and simple initial endpoint

--- server.js
+++ server.js
@@ -306,6 +306,23 @@ router.get('/api/addUser', function(req, res) {
 });
 
 
+var captchapng = require('captchapng');
+router.get('/api/captcha', function(req, res) {
+  var p = new captchapng(80,30,parseInt(Math.random()*9000+1000)); // width,height,numeric captcha 
+  p.color(0, 0, 0, 0);  // First color: background (red, green, blue, alpha) 
+  p.color(80, 80, 80, 255); // Second color: paint (red, green, blue, alpha) 
+
+  var img = p.getBase64();
+  var imgbase64 = new Buffer(img,'base64');
+  res.writeHead(200, {
+      'Content-Type': 'image/png'
+  });
+  res.end(imgbase64);
+});
+
+
+
+
 
 server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function() {
   console.log('trying to listen...');
