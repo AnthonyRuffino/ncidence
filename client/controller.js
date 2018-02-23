914f7d1b7b02b445e7864e3b33f047463aac8921
Anthony Ruffino <aruffino84@gmail.com>
2018-02-23 15:49:21 +0000

    socket io chat/play controller refactor

---
+++ client/controller.js
@@ -0,0 +1,70 @@
+'use strict'
+
+class Controller {
+    constructor({ name }) {
+        this.name = name;
+        this.subdomain;
+    }
+    connect({ io, $scope }) {
+        const socket = io.connect();
+
+        $scope.messages = [];
+        $scope.roster = [];
+        $scope.name = '';
+        $scope.text = '';
+        $scope.title = '';
+        $scope.owner = '';
+        
+        const hook = {
+          connect: () => {},
+          whoami: () => {},
+          message: () => {},
+          connected: () => {},
+          roster: () => {}
+        }
+
+        socket.on('connect', () => {
+        	//console.log('connect');
+        	hook.connect();
+        });
+        
+        socket.on('whoami', (me) => {
+        	console.log('me', me);
+        	hook.whoami(me);
+        });
+
+        socket.on('message', (msg) => {
+          $scope.messages.push(msg);
+          $scope.$apply();
+          hook.message(msg);
+        });
+        
+        socket.on('connected', (data) => {
+          this.subdomain = data.subdomain;
+          console.log('data.subdomain', data.subdomain);
+          $scope.title = this.name + ' ' + data.subdomain;
+          console.log('game', data.owner);
+          $scope.$apply();
+          hook.connected(data);
+        });
+
+        socket.on('roster', (names) => {
+          $scope.roster = names;
+          $scope.$apply();
+          hook.roster(names);
+        });
+
+        $scope.send = () => {
+          socket.emit('message', $scope.text);
+          $scope.text = '';
+        };
+        
+        return { 
+          name: this.name,
+          subdomain: this.subdomain,
+          emit: socket.emit,
+          on: socket.on,
+          hook 
+        };
+      }
+}
--- utils/middleware/hostCookie.js
+++ utils/middleware/hostCookie.js
@@ -4,7 +4,7 @@ module.exports = function(cookieName, maxAge) {
     let setHostCookie = (req, res, next) => {
       let cookieValue = req.cookies[cookieName];
       if (cookieValue === undefined || cookieValue === null) {
-        cookieValue = require(global.__base + 'utils/guid.js').generate(true);
+        cookieValue = require('uuid/v4')();
       }
       res.cookie(cookieName, cookieValue, { maxAge: maxAge, httpOnly: true, domain: '.' + global.__host });
       next();
