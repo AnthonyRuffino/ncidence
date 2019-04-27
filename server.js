91b6d56b66d7ccfaa2647ee131ed456c1a85e07c
Anthony Ruffino <aruffino84@gmail.com>
2019-04-27 00:53:38 +0000

    simpler sockethub and no subdomain content

--- server.js
+++ server.js
@@ -5,16 +5,18 @@
  / /|  / /__/ / /_/ /  __/ / / / /__/  __/
 /_/ |_/\___/_/\__,_/\___/_/ /_/\___/\___/ 
 */
-const constants = require('./constants');
+const LiteLifting = require('lite-lifting');
+
+const constants = { host: 'ncidence-aruffino.c9users.io' };
+constants.schema = LiteLifting.schema(constants.host);
+
 global.__rootdir = __dirname + '/';
 global.__publicdir = __dirname + '/client/';
-require(global.__publicdir + 'asciiArt.js')();
 
-//const LiteLifting = require('lite-lifting');
-const LiteLifting = require('lite-lifting');
 const liteLiftConfig = {
   appName: 'ncidence',
-  schema: 'ncidence__aruffino_c9users_io',
+  publicdir: global.__publicdir,
+  schema: constants.schema,
   useLoggerPlusPlus: true,
   port: process.env.PORT,
   securePort: process.env.SECURE_PORT,
@@ -42,15 +44,7 @@ const liteLift = new LiteLifting(liteLiftConfig);
 
 
 const {
-  express,
-  formidable,
-  jwtCookiePasser,
-  router,
-  server,
-  secureServer,
   storming,
-  urlencodedParser,
-  userService,
   yourSql
 } = { ...liteLift };
 
@@ -60,150 +54,33 @@ const gameService = require('./utils/orm/services/gameService.js')({
   secrets: liteLiftConfig
 });
 
+
 liteLift.sync(start);
 
 function start(err) {
   err && console.log('ERROR passed to start method: ' + err);
-  //////////////////////
-  // BEGIN SERVICES
-  //////////////////////
-
-  setTimeout(() => {
-    //TODO: FIx ORM helper so this data is here by now on first start-up
-    userService.getUserByUsername('admin', adminUser => {
-      if (adminUser) {
-        console.info("Creating Test Game");
-        gameService.createGameAndSchema({
-          name: 'test',
-          userId: adminUser.id,
-          ignoreTestExists: true
-        });
-      }
-      else {
-        console.error("ADMIN USER MISSING!!");
-      }
-    });
-  }, 10000);
-
-
-  //////////////////////
-  // END SERVICES
-  //////////////////////
-
-
-
-  // Driver middleware
-  const contentFromDb = new(require('./utils/middleware/contentFromDb.js'))(constants, gameService, {
-    ['/driver.js']: 'driver', ['/common.js']: 'common' });
-  router.use('/', (req, res, next) => { contentFromDb.handle(req, res, next); });
-
-  // File system middleware
-  router.use(require('no-extension')(global.__publicdir));
-  router.use(express.static(global.__publicdir));
-
-
-
-
-  ///////////////////////////////////////////
-  // BEGIN SOCKET IO SETUP & JWT AUTH SETUP///
-  ////////////////////////////////////////////
-  console.log('---Socket IO');
-  let SocketHub = require('socket-hub');
   
-  var SrcString = require('src-string');
-  const srcString = new SrcString({
-    contentSrc: {
-      getEntity: ({subdomain, type, filter = { version: 'test' }} ) => {
-        subdomain !== '#' && gameService.getGameEntityRecord(subdomain, type, filter);
-      }
+  liteLift.configureSocketHub({
+    getEntity: ({ subdomain, type, filter = { version: 'test' } }) => {
+      subdomain !== '#' && gameService.getGameEntityRecord(subdomain, type, filter);
+    },
+    rootHost: constants.host,
+    getStorming: ({ subdomain, refreshCache = false }) =>
+      gameService.fetchCachedGameOrmHelper({ gameName: subdomain, refreshCache }),
+    getInfo: async(subdomain) => {
+      let gameAndDatabaseTemp = await gameService.getGameAndDatabase(subdomain);
+      const game = gameAndDatabaseTemp && gameAndDatabaseTemp.game ? gameAndDatabaseTemp.game : null;
+      return {
+        subdomain: subdomain,
+        game,
+        owner: (game ? game.owner.username : 'admin')
+      };
     }
   });
   
-  
-  let socketIOHelper = new SocketHub({
-    appName: 'ncidence',
-    server: secureServer ? secureServer : server,
-    tokenUtil: jwtCookiePasser,
-    rootHost: constants.host,
-    storming,
-    
-    subdomainContent: {
-      getInfo: async(subdomain) => {
-        let gameAndDatabaseTemp = await gameService.getGameAndDatabase(subdomain);
-        const game = gameAndDatabaseTemp && gameAndDatabaseTemp.game ? gameAndDatabaseTemp.game : null;
-        return {
-          subdomain: subdomain,
-          game,
-          owner: (game ? game.owner.username : 'admin')
-        };
-      },
-      getStorming: ({subdomain, refreshCache = false}) =>
-        gameService.fetchCachedGameOrmHelper({gameName: subdomain, refreshCache })
-      ,
-      getAlternateContent: (context) => srcString.getContent(context),
-      getContent: ({subdomain, type}) => {
-        const filePath = (type === 'common' ? global.__publicdir : global.__rootdir) + type + '.js';
-		  	console.info(`[${subdomain}] - Loading default exports for ${type}. filePath: ${filePath}`);
-		  	return require(filePath);
-      }
-    },
-  });
-  socketIOHelper.init();
-  gameService.setSocketIOHelper(socketIOHelper);
-  liteLift.socketBuddy = socketIOHelper;
-  /////////////////////////////////////////
-  // END SOCKET IO SETUP & JWT AUTH SETUP///
-  /////////////////////////////////////////
+  gameService.setSocketIOHelper(liteLift.socketHub);
 
   liteLift.start(() => {
-    router.post('/uploadFrontend', jwtCookiePasser.authRequired(), (req, res) => {
-      let form = new formidable.IncomingForm();
-      contentFromDb.updateGameFile(form, 'driver', req, res);
-    });
-
-    router.post('/uploadBackend', jwtCookiePasser.authRequired(), (req, res) => {
-      let form = new formidable.IncomingForm();
-      contentFromDb.updateGameFile(form, 'backend', req, res);
-    });
-
-    router.post('/uploadCommon', jwtCookiePasser.authRequired(), (req, res) => {
-      let form = new formidable.IncomingForm();
-      contentFromDb.updateGameFile(form, 'common', req, res);
-    });
-
-    router.post('/createGame', jwtCookiePasser.authRequired(), urlencodedParser, function(req, res) {
-      gameService.createGameAndSchema({
-        name: req.body.game,
-        userId: req.user.id,
-      }).then(game => {
-        let port = constants.getPort(req.get('host'));
-        port = port === '80' || !port ? '' : `:${port}`;
-        res.redirect(req.protocol + '://' + req.body.game + '.' + constants.host + port);
-        socketIOHelper.clearFromSubdomainInfoMap(req.body.game);
-      }).catch(err => {
-        res.json(500, { err });
-      });
-    });
-
-    router.get('/api/login', function(req, res) {
-      userService.login(req, res);
-    });
-
-    router.get('/api/addUser', function(req, res) {
-      userService.createUser(req, res);
-    });
-
-
-    router.get("/public", function(req, res) {
-      res.json({ message: "Public Success!", user: req.user });
-    });
-
-    router.get("/secret", jwtCookiePasser.authRequired(), function(req, res) {
-      res.json({ message: "Secret Success!", user: req.user });
-    });
+    console.log('Start-up complete.');
   });
-
-
-
-
 }
